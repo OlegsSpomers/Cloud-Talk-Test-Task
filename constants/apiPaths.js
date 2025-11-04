eb4d14cf90c6761e519857e8da4e67d5b2678d6b
oshpomer <oshpomer@upteam.com>
2025-11-04 23:07:35 +0100

    splited tests and added apipaths together with base url

---
+++ constants/apiPaths.js
@@ -0,0 +1,2 @@
+export const UNIX_TIMESTAMP_CONVERTER_PATH = '/api/unix-timestamp-converter';
+export const QUERY_FLAG = '?cached';
--- tests/positiveApi.spec.js
+++ tests/positiveApi.spec.js
@@ -1,14 +1,11 @@
 import { test, expect } from '@playwright/test';
+import { UNIX_TIMESTAMP_CONVERTER_PATH, QUERY_FLAG } from '../constants/apiPaths';
 
 test.describe('API /api/unix-timestamp-converter/', () => {
 
-    const BASE_URL = 'https://helloacm.com/api/unix-timestamp-converter/';
-    const QUERY_FLAG = '?cached';
-
     test('Convert timestamp 0 to date', async ({ request }) => {
 
-
-        const response = await request.get(BASE_URL + QUERY_FLAG, {
+        const response = await request.get(UNIX_TIMESTAMP_CONVERTER_PATH + QUERY_FLAG, {
             params: {
                 s: 0
             }
@@ -26,30 +23,14 @@ test.describe('API /api/unix-timestamp-converter/', () => {
 
     test('Convert string date to Timestamp', async ({ request }) => {
 
-        const response = await request.get(BASE_URL, {
+        const response = await request.get(UNIX_TIMESTAMP_CONVERTER_PATH + QUERY_FLAG, {
             params: {
                 s: '2000-01-01 00:00:00'
             }
         });
 
-        expect(response.ok()).toBeTruthy();
+        expect(response).toBeOK();
         const text = await response.text();
         expect(text).toBe('946684800');
     });
-
-
-    test('Invalid request', async ({ request }) => {
-
-        const response = await request.get(BASE_URL, {
-            params: {
-                s: 'это точно не дата'
-            }
-        });
-
-        expect(response.ok()).toBeTruthy();
-        const text = await response.text();
-
-        expect(text).toContain('false');
-    });
-
 });
